ffbb9998b5a61bcc3107a06d82c53029a02cac63
Juwon Hong <juwonhp373@gmail.com>
2018-09-04 21:17:34 +0900

    modified

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -2,7 +2,7 @@
 
 import Vue from 'vue';
 import Hello from '@/page/Hello';
-
+// Hello.vue를 $mount시켰다는건 구동을 시킨것, .hello h1의 텍스트가 'Welcome to Hanu Vue Template'이길 기대한다.
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Hello);
